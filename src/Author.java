c114bf2b69e71b5995cefd15c7c72307e6c7a8eb
Amir Makhmudov <amir.makhmudov.f@gmail.com>
2025-12-12 22:08:17 +0300

    feat: создать класс Author

---
+++ src/Author.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Author {
+    private final int id;
+    private final List<Book> booksWritten;
+    private String name;
+
+    public Author(int id, String name) {
+        this.id = id;
+        this.booksWritten = new ArrayList<>();
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public List<Book> getBooksWritten() {
+        return booksWritten;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
